bc6ef27a99b8dec05d37ebc679d7575297a108e9
tyler <tylerscottwatson@gmail.com>
2024-10-10 11:03:18 -0700

    Initial Commit

---
+++ MeshGradient/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MeshGradient
+//
+//  Created by Tyler Watson on 10/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MeshGradient/MeshGradientApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeshGradientApp.swift
+//  MeshGradient
+//
+//  Created by Tyler Watson on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MeshGradientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
